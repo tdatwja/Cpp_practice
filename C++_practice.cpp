57b6124b04b1b7cd850d27acc638d37d2d76ee86
misaki <99859593+tdatwja@users.noreply.github.com>
2022-07-24 07:48:17 +0900

    Add files via upload

---
+++ C++_practice.cpp
@@ -0,0 +1,19 @@
+# include < bits / stdc ++.h >
+using namespace std;
+
+int main () {
+    cout << "Hello,world!" << endl;
+}
+
+int main () {
+    cout << "a" << "b" << endl;
+}
+
+int main ()  [
+    int name;
+    name = 2;
+    cout << name + 5 << endl;
+    cout << name - 1 << endl;
+    cout << name * 4 << endl;
+    cout << name / 2 << endl;
+]
